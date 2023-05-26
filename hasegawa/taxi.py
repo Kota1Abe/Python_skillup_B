eb909323f51519ba7ee704a1099aa695c796de8d
TakumiHasegawa321 <hasegawa.takumi.rf@mynavi.jp>
2023-05-26 02:38:36 +0000

    modify branch

--- hasegawa/taxi.py
+++ hasegawa/taxi.py
@@ -4,7 +4,18 @@ args = sys.argv
 
 traveled_distance = int(args[1])
 
-intial_fare = 650
+intial_fare = 630
 distance_fare = 98
+distance_fare_rate = 344
 
-# if traveled_distance <= 1500
+
+# 初乗りタクシー運賃をtaxi_fareに代入
+taxi_fare = 0
+
+if 1 <= traveled_distance <= 1500:
+    taxi_fare = intial_fare
+else:
+    taxi_fare = intial_fare + 
+
+print(taxi_fare)
+
