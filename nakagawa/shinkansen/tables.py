f1c094e764c1844ae2bd4884dab546a9deaea7db
nakayuna27 <nakagawa.yuna.xa@mynavi.jp>
2023-05-26 08:48:51 +0000

    add: stations data

--- nakagawa/shinkansen/tables.py
+++ nakagawa/shinkansen/tables.py
@@ -3,6 +3,7 @@ from sqlalchemy import Column,String, Numeric, Integer
 from database import Base
 from database import ENGINE
 
+#
 class Stations(Base):
     __tablename__ = 'stations'
     seq = Column('seq', Integer, primary_key=True)
