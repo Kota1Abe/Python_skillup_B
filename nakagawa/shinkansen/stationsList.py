52e8d93dcd3c760d6c8cdf7e19e801dfa8310943
nakayuna27 <nakagawa.yuna.xa@mynavi.jp>
2023-05-29 01:12:17 +0000

    refactor

--- nakagawa/shinkansen/stationsList.py
+++ nakagawa/shinkansen/stationsList.py
@@ -1,9 +1,8 @@
 from database import session
 from tables import Stations
 import csv
-from datetime import date
 
-#holiday.csvを読み込む
+#stations.csvを読み込む
 filename = '/home/matcha-23training/Python_skillup_B/nakagawa/shinkansen/data/stations.csv'
 with open(filename, encoding='utf-8_sig', newline='') as f:
     csvreader = csv.reader(f)
