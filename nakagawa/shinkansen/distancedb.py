7f2560d7c131d1fb114ac2a42fd099483c759be2
nakayuna27 <nakagawa.yuna.xa@mynavi.jp>
2023-05-29 01:53:56 +0000

    add: 駅間計算

--- nakagawa/shinkansen/distancedb.py
+++ nakagawa/shinkansen/distancedb.py
@@ -1,1 +1,16 @@
 import sys
+args = sys.argv
+from database import session
+from tables import Stations
+
+#出発駅と到着駅を入力
+departure_station = args[1]
+arrival_station = args[2]
+
+departure_distance = session.query(Stations.kilo).filter(Stations.name == departure_station).first()
+arrival_distance = session.query(Stations.kilo).filter(Stations.name == arrival_station).first()
+
+#駅間の距離を計算
+distance = arrival_distance[0] - departure_distance[0]
+print(distance)
+
